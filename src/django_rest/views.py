27b846862b7a7f931c6e9503b320773c40c6acbf
Daniel Quinn <me@danielquinn.org>
2012-05-20 09:54:54 +0200

    This project has become something of a beast as a result of these edits.  I'm reasonably sure that now that tastypie is improving, I'll be migrating my code to that.  I'll have to check it out first

--- src/django_rest/views.py
+++ src/django_rest/views.py
@@ -1,189 +1,243 @@
 class RestView(object):
-    """
-        A custom REST server to keep things simpler and more extensible than
-        say, django-piston.
-    """
+	"""
+		A custom REST server to keep things simpler and more extensible than
+		say, django-piston.
+	"""
 
-    #
-    # Ignore CSRF rules since APIs don't have to worry about such things.
-    # Note that I tried to use the decorator, but it didn't seem to like
-    # class-based views.
-    #
-    csrf_exempt = True
+	#
+	# Ignore CSRF rules since APIs don't have to worry about such things.
+	# Note that I tried to use the decorator, but it didn't seem to like
+	# class-based views.
+	#
+	csrf_exempt = True
 
-    request = None
+	request = None
 
-    def __call__(self, request, *a, **kwa):
+	def __call__(self, request, *a, **kwa):
 
-        method = kwa["__method"]
-        del(kwa["__method"])
+		from django.conf import settings
 
-        self.request = request
-        self._setup_data()
+		method = kwa["__method"]
+		del(kwa["__method"])
 
-        from django.core.exceptions import ValidationError, PermissionDenied
-        from django.http import HttpResponseServerError, HttpResponseNotAllowed, HttpResponseBadRequest, Http404, HttpResponseNotFound
+		self.request = request
+		self._setup_data()
 
-        try:
+		# Check for the sexy header
+		if "HTTP_X_SEXY" in self.request.META:
+			self.sexy = True
 
-            params = self._authenticate_signature() # Returns parameters without the oauth_* stuff
+		from django.core.exceptions import ValidationError, PermissionDenied
+		from django.http import HttpResponseServerError, HttpResponseNotAllowed, HttpResponseBadRequest, Http404, HttpResponseNotFound, HttpResponse
 
-            response = getattr(self, method)(*a, **kwa)
-            response.content = response.content
-            return response
+		try:
 
-        except Http404:
-            return HttpResponseNotFound(
-                self._render("Not Found")
-            )
+			params = self._authenticate_signature() # Returns parameters without the oauth_* stuff
 
-        except ValidationError as e:
-            return HttpResponseBadRequest(
-                self._render(e.messages[0])
-            )
+			if settings.REST_USE_CORS and request.method == "OPTIONS":
 
-        except PermissionDenied as e:
-            if type(e.message) == tuple: # Bad Method
-                return HttpResponseNotAllowed(
-                    self._render(e.message)
-                )
-            else:
-                return HttpResponseBadRequest(
-                    self._render(e.message)
-                )
+				response = HttpResponse()
 
-        except Exception as e:
+			else:
 
-            import sys,traceback
+				print (request.method, type(self), getattr(self, method), a, kwa, request.DATA)
+				response = getattr(self, method)(*a, **kwa)
+				response.content = response.content
 
-            from django.conf import settings
+			if settings.REST_USE_CORS:
+				response["Access-Control-Allow-Origin"] = "*"
+				response["Access-Control-Allow-Credentials"] = "true"
+				response["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE"
+				response["Access-Control-Max-Age"] = "1728000"
+				response["Access-Control-Allow-Headers"] = "authorization"
 
-            # TODO: log this sort of thing
-            print "\n\nUnhandled exception:\n  %s\n\n" % e
-            traceback.print_exc(file=sys.stdout)
+			return response
 
-            if settings.DEBUG:
-                return HttpResponseServerError(self._render(u"Server error: %s" % e))
-            else:
-                return HttpResponseServerError(
-                    self._render("Server Error.  Please contact support")
-                )
+		except Http404:
+			return HttpResponseNotFound(
+				self._render("Not Found")
+			)
 
+		except ValidationError as e:
+			return HttpResponseBadRequest(
+				self._render(e.messages[0])
+			)
 
-    def OK(self, content):
-        from django.http import HttpResponse
-        return HttpResponse(content=self._render(content), status=200, content_type="application/json")
+		except PermissionDenied as e:
+			if type(e.message) == tuple: # Bad Method
+				return HttpResponseNotAllowed(
+					self._render(e.message)
+				)
+			else:
+				return HttpResponseBadRequest(
+					self._render(e.message)
+				)
 
+		except Exception as e:
 
-    def CREATED(self, content):
-        from django.http import HttpResponse
-        return HttpResponse(content=self._render(content), status=201, content_type="application/json")
+			import sys,traceback
 
+			# TODO: log this sort of thing
+			print "\n\nUnhandled exception:\n  %s\n\n" % e
+			traceback.print_exc(file=sys.stdout)
 
-    def UPDATED(self):
-        from django.http import HttpResponse
-        return HttpResponse(status=204)
-
-
-    def DELETED(self):
-        from django.http import HttpResponse
-        return HttpResponse(status=204)
-
-
-    def _setup_data(self):
-        if self.request.method == "GET":
-            self.request.DATA = self.request.GET
-        elif self.request.method == "POST":
-            self.request.DATA = self.request.POST
-        elif self.request.method in ("PUT","DELETE",):
-            from urlparse import parse_qs
-            data = dict(parse_qs(self.request.raw_post_data).items())
-            for k,v in data.items():
-                if len(v) == 1:
-                    data[k] = v.pop()
-            self.request.DATA = data
+			if settings.DEBUG:
+				return HttpResponseServerError(self._render(u"Server error: %s" % e))
+			else:
+				return HttpResponseServerError(
+					self._render("Server Error.  Please contact support")
+				)
 
 
-    def _render(self, payload):
-        """
-            TODO: Switch on format
-        """
-        return self._output_json(payload)
+	def OK(self, content):
+		from django.http import HttpResponse
+		return HttpResponse(content=self._render(content), status=200, content_type="application/json")
 
 
-    def _output_json(self, payload):
-
-        from django.core.serializers.json import simplejson
-
-        if "callback" in self.request.DATA:
-            return self.request.DATA["callback"] + "(%s);" % simplejson.dumps(payload)
-
-        return simplejson.dumps(payload)
+	def CREATED(self, content):
+		from django.http import HttpResponse
+		return HttpResponse(content=self._render(content), status=201, content_type="application/json")
 
 
-    def _validate_request(self, required):
-
-        from django.core.exceptions import ValidationError
-
-        check = self.request.DATA
-        for k in required.keys():
-            if k not in check:
-                raise ValidationError("This method requires the following arguments: %s" % ", ".join(required))
-            if required[k] == int and not check[k].isdigit():
-                raise ValidationError("\"%s\" must be numeric" % k)
+	def UPDATED(self):
+		from django.http import HttpResponse
+		return HttpResponse(status=204)
 
 
-    def _check_authenticated(self):
-
-        from django.core.exceptions import PermissionDenied
-
-        if not self.request.user.is_authenticated():
-            raise PermissionDenied("You must be logged in to do that.")
+	def DELETED(self):
+		from django.http import HttpResponse
+		return HttpResponse(status=204)
 
 
-    def _authenticate_signature(self):
-        """
-            Basic OAuth authentication.  Thankfully, we don't have to
-            implement the full 3-legged authentication process as this is a
-            closed system.
-        """
+	def _setup_data(self):
 
-        from django.conf import settings
+		self.request.DATA = []
 
-        if not hasattr(settings, "REST_USE_OAUTH") or not settings.REST_USE_OAUTH:
-            return self.request.REQUEST
+		if self.request.method == "GET":
+			self.request.DATA = self.request.GET
+		elif self.request.method == "POST":
+			self.request.DATA = self.request.POST
+		elif self.request.method in ("PUT","DELETE",):
+			from urlparse import parse_qs
+			data = dict(parse_qs(self.request.raw_post_data).items())
+			for k,v in data.items():
+				if len(v) == 1:
+					data[k] = v.pop()
+			self.request.DATA = data
 
-        import re, oauth2
 
-        from django.core.exceptions import ValidationError
-        from django.db.utils import IntegrityError
+	def _render(self, payload):
+		"""
+			TODO: Switch on format
+		"""
+		return self._output_json(payload)
 
-        from django_rest.models import Consumer, Nonce
 
-        consumer = Consumer.objects.get(pk=1)
+	def _output_json(self, payload):
 
-        server = oauth2.Server(signature_methods={'HMAC-SHA1': oauth2.SignatureMethod_HMAC_SHA1()})
-        token  = oauth2.Token(key=consumer.access_key, secret=consumer.access_sec)
+		from django.core.serializers.json import simplejson
 
-        req = None
-        if self.request.method == "POST":
-            req = oauth2.Request(method=self.request.method, url=self.request.build_absolute_uri(), parameters=self.request.POST)
-        elif self.request.method == "GET":
-            req = oauth2.Request(method=self.request.method, url=re.sub(r"\?.*", "", self.request.build_absolute_uri()), parameters=self.request.GET)
+		if "callback" in self.request.DATA:
+			return self.request.DATA["callback"] + "(%s);" % simplejson.dumps(payload)
 
-        # Nonce Checking
-        n = self.request.REQUEST.get("oauth_nonce")
-        if not n:
-            raise ValidationError("OAuth failure: oauth_noce")
+		if hasattr(self, "sexy"):
+			return simplejson.dumps(payload, indent=4)
 
-        nonce = Nonce(key=n)
-        try:
-            nonce.save() # This will fail on a key error if it's already in the database
-        except IntegrityError:
-            raise ValidationError("Nonce is no longer valid")
+		return simplejson.dumps(payload)
 
-        # Signature Checking
-        try:
-            return server.verify_request(req, consumer, token) # Assuming the nonce passes, now we validate the key
-        except Exception as e:
-            raise ValidationError("OAuth failure: \n%s" % e)
+
+	def _validate_request(self, required):
+
+		from django.core.exceptions import ValidationError
+
+		check = self.request.DATA
+		for k in required.keys():
+
+			if k not in check:
+				raise ValidationError("This method requires the following arguments: %s" % ", ".join(required))
+
+			if required[k] == int:
+				try:
+					for element in check[k]:
+						int(element)
+				except ValueError:
+					raise ValidationError("\"%s\" must be numeric" % k)
+
+			elif required[k] == float:
+				try:
+					for element in check[k]:
+						float(element)
+				except ValueError:
+					raise ValidationError("\"%s\" must be a floating point number")
+
+
+	def _check_authenticated(self):
+		"""
+			This is Bad Form.  You don't setup the session in the same method
+			in which you check its validity.
+		"""
+
+		from base64 import b64decode
+		from django.core.exceptions import PermissionDenied
+		from django.contrib.auth import authenticate, login
+
+		try:
+			uname, passwd = b64decode(self.request.META["HTTP_AUTHORIZATION"].split()[1]).split(':')
+			user = authenticate(username=uname, password=passwd)
+			if user is not None:
+				if user.is_active:
+					login(self.request, user)
+					self.request.user = user
+		except KeyError:
+			pass
+
+		if not self.request.user.is_authenticated():
+			raise PermissionDenied("You must be logged in to do that.")
+
+
+	def _authenticate_signature(self):
+		"""
+			Basic OAuth authentication.  Thankfully, we don't have to
+			implement the full 3-legged authentication process as this is a
+			closed system.
+		"""
+
+		from django.conf import settings
+
+		if not hasattr(settings, "REST_USE_OAUTH") or not settings.REST_USE_OAUTH:
+			return self.request.REQUEST
+
+		import re, oauth2
+
+		from django.core.exceptions import ValidationError
+		from django.db.utils import IntegrityError
+
+		from django_rest.models import Consumer, Nonce
+
+		consumer = Consumer.objects.get(pk=1)
+
+		server = oauth2.Server(signature_methods={'HMAC-SHA1': oauth2.SignatureMethod_HMAC_SHA1()})
+		token  = oauth2.Token(key=consumer.access_key, secret=consumer.access_sec)
+
+		req = None
+		if self.request.method == "POST":
+			req = oauth2.Request(method=self.request.method, url=self.request.build_absolute_uri(), parameters=self.request.POST)
+		elif self.request.method == "GET":
+			req = oauth2.Request(method=self.request.method, url=re.sub(r"\?.*", "", self.request.build_absolute_uri()), parameters=self.request.GET)
+
+		# Nonce Checking
+		n = self.request.REQUEST.get("oauth_nonce")
+		if not n:
+			raise ValidationError("OAuth failure: oauth_noce")
+
+		nonce = Nonce(key=n)
+		try:
+			nonce.save() # This will fail on a key error if it's already in the database
+		except IntegrityError:
+			raise ValidationError("Nonce is no longer valid")
+
+		# Signature Checking
+		try:
+			return server.verify_request(req, consumer, token) # Assuming the nonce passes, now we validate the key
+		except Exception as e:
+			raise ValidationError("OAuth failure: \n%s" % e)
